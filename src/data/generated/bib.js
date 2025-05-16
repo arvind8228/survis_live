41a495a7661a21288f88837563a869cec27b94c3
fahadmujawar <fahadmujawar002@gmail.com>
2025-05-16 14:35:07 +0100

    Initial commit

---
+++ src/data/generated/bib.js
@@ -0,0 +1,92 @@
+﻿const generatedBibEntries = {
+    "boyle2023spatiotemporal": {
+        "author": "Boyle, David and Kalita, Jugal",
+        "journal": "arXiv preprint arXiv:2305.03835",
+        "title": "Spatiotemporal transformer for stock movement prediction",
+        "type": "article",
+        "url": "https://arxiv.org/abs/2305.03835",
+        "year": "2023"
+    },
+    "hu2021stock": {
+        "author": "Hu, Xiaoyu",
+        "booktitle": "2021 International Conference on Machine Learning, Big Data and Business Intelligence (MLBDBI)",
+        "doi": "10.1109/MLBDBI54094.2021.00019",
+        "pages": "110--114",
+        "publisher": "IEEE",
+        "title": "Stock price prediction based on temporal fusion transformer",
+        "type": "inproceedings",
+        "year": "2021"
+    },
+    "ko2021lstm": {
+        "author": "Ko, Chun-Ru and Chang, Hsiu-Ting",
+        "doi": "10.7717/peerj-cs.408",
+        "journal": "PeerJ Computer Science",
+        "pages": "e408",
+        "title": "LSTM-based sentiment analysis for stock price forecast",
+        "type": "article",
+        "volume": "7",
+        "year": "2021"
+    },
+    "lim2019temporal": {
+        "author": "Lim, Bryan and Arik, Sercan {\\\"O}mer and Loeff, Nicolas and Pfister, Tomas",
+        "journal": "arXiv preprint arXiv:1912.09363",
+        "title": "Temporal fusion transformers for interpretable multi-horizon time series forecasting",
+        "type": "article",
+        "url": "https://arxiv.org/abs/1912.09363",
+        "year": "2019"
+    },
+    "pei2025cross": {
+        "author": "Pei, Yuyang and Cartlidge, John and Mandal, Abhishek and Gold, David and Marcilio, Eduardo and Mazzon, Roberto",
+        "journal": "arXiv preprint arXiv:2504.13522",
+        "title": "Cross-modal temporal fusion for financial market forecasting",
+        "type": "article",
+        "url": "https://arxiv.org/abs/2504.13522",
+        "year": "2025"
+    },
+    "peik2024leveraging": {
+        "author": "Peik, Amir and Chahooki, Mahmood A. Zare and Milani Fard, Amir and Agha Sarram, Mahdi",
+        "journal": "arXiv preprint arXiv:2412.14529",
+        "title": "Leveraging Time Series Categorization and Temporal Fusion Transformers to Improve Cryptocurrency Price Forecasting",
+        "type": "article",
+        "url": "https://arxiv.org/abs/2412.14529",
+        "year": "2024"
+    },
+    "wen2022survey": {
+        "author": "Wen, Qingsong and Zhou, Tian and Zhang, Cheng and Chen, Wei and Ma, Ziqing and Yan, Jiajun and Sun, Lifan",
+        "journal": "arXiv preprint arXiv:2202.07125",
+        "title": "Transformers in time series: A survey",
+        "type": "article",
+        "url": "https://arxiv.org/abs/2202.07125",
+        "year": "2022"
+    },
+    "xiao2024comparative": {
+        "author": "Xiao, Jia and Deng, Ting and Bi, Shuming",
+        "doi": "10.48550/arXiv.2411.05790",
+        "journal": "arXiv preprint arXiv:2411.05790",
+        "title": "Comparative analysis of LSTM, GRU, and transformer models for stock price prediction",
+        "type": "article",
+        "year": "2024"
+    },
+    "yang2021hybrid": {
+        "author": "Yang, Yu and Yang, Yang and Zhou, Wei",
+        "journal": "Soft Computing",
+        "note": "Available on ResearchGate",
+        "number": "21",
+        "pages": "12368--12383",
+        "title": "Research on a hybrid prediction model for stock price based on long short-term memory and variational mode decomposition",
+        "type": "article",
+        "url": "https://www.researchgate.net/publication/354388598",
+        "volume": "25",
+        "year": "2021"
+    },
+    "yang2022lasso": {
+        "author": "Yang, Jia and Wang, Yu and Li, Xin",
+        "doi": "10.7717/peerj-cs.1148",
+        "journal": "PeerJ Computer Science",
+        "pages": "e1148",
+        "title": "Prediction of stock price direction using the LASSO-LSTM model combining technical indicators and financial sentiment analysis",
+        "type": "article",
+        "volume": "8",
+        "year": "2022"
+    }
+};
